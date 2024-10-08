edaecbdc9a38058635f9e8243f46a803f7787127
Vi Trung <vitrung06022004@gmail.com>
2024-10-08 23:23:36 +0700

    04.update handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+const path = require("path");
 const express = require("express");
 // import morgan
 const morgan = require("morgan");
 // import Handlebars
-const { engine } = require("express-handlebars");
+const handlebars = require("express-handlebars");
 
 const app = express();
 const port = 8080;
@@ -10,18 +11,25 @@ const port = 8080;
 // use morgan
 app.use(morgan("combined"));
 
-// Template engine 
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+// Template engine
+app.engine(
+  "hbs",
+  handlebars.engine({
+    // rút ngắn tên đuôi file handlebars thành hbs
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "resources/views/layouts"), // Đường dẫn đến layouts
+    partialsDir: path.join(__dirname, "resources/views/partials"),
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+app.set("partials", path.join(__dirname, "resources/partials"));
 
 app.get("/home", (req, res) => {
-  res.send(
-    // render HTML
-    `
-    <p>Return</p>
-    <h1>Hello world!</h1>
-    `
-  );
+  res.render("home");
+});
+app.get("/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(port, () => {
