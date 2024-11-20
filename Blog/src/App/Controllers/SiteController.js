d6e852ea81e080be187430cd98f6b78d4ee21590
Vi Trung <vitrung06022004@gmail.com>
2024-11-20 10:41:24 -0800

    [CRUD] Read form database. Create home page

--- Blog/src/App/Controllers/SiteController.js
+++ Blog/src/App/Controllers/SiteController.js
@@ -1,16 +1,28 @@
 const Course = require('../Models/Course');
 
 class SiteController {
+    // Cách 1
     // [GET] /home
-    async index(req, res) {
+    /*
+    async index(req, res,next) {
         try {
             const courses = await Course.find({});
             res.json(courses);
         } catch (err) {
-            res.status(400).json({ error: 'Message!!!' });
+            next(err);
         }
     }
-
+*/
+    // Cách 2
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) =>
+                res.render('home', {
+                    courses: courses,
+                }),
+            )
+            .catch((error) => next(error));
+    }
     // [GET] /form
     form(req, res) {
         res.render('form');
