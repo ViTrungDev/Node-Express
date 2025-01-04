708bdc5f5d768f4487a10bb6bd6872d82619342f
Trung <133032056+ViTrungDev@users.noreply.github.com>
2025-01-05 00:00:18 +0700

    .

--- Blogs/src/router/Product.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const HomeController = require("../Controller/HomeController");
-
-router.get("/", HomeController.index);
-router.get("/:slug", HomeController.show);
-module.exports = router;
--- Blogs/src/router/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productRouter = require("./Product");
-
-// Route cho đường dẫn gốc "/"
-router.use("/", productRouter);
-
-module.exports = router;
