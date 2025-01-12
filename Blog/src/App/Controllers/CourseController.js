d884309a4cab2c778259e85f3cbda729fc2a3ea6
Trung <133032056+ViTrungDev@users.noreply.github.com>
2025-01-12 17:12:58 +0700

    [CRUD] finish select

--- Blog/src/App/Controllers/CourseController.js
+++ Blog/src/App/Controllers/CourseController.js
@@ -65,6 +65,18 @@ class CourseController {
       .then(() => res.redirect("back"))
       .catch(next);
   }
+  // [POST] /course/handle-form-action
+  handleFormAction(req, res, next) {
+    switch (req.body.action) {
+      case "delete":
+        Course.delete({ _id: { $in: req.body.courseIds } })
+          .then(() => res.redirect("back"))
+          .catch(next);
+        break;
+      default:
+        res.json({ message: "Action is invalid!" });
+    }
+  }
 }
 
 module.exports = new CourseController();
--- Blog/src/routes/course.js
+++ Blog/src/routes/course.js
@@ -5,6 +5,7 @@ const CourseController = require("../App/Controllers/CourseController");
 router.get("/create", CourseController.create);
 router.post("/store", CourseController.store);
 router.get("/:id/edit", CourseController.edit);
+router.post("/handle-form-action", CourseController.handleFormAction);
 router.put("/:id", CourseController.update);
 router.patch("/:id/restore", CourseController.restore);
 router.delete("/:id", CourseController.destroy);
